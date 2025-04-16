8f5c127602362081a8da2a5634b3262183a9d6f6
Breno Leal <copywriting.leal@gmail.com>
2025-04-16 09:49:17 -0300

    chore: atualização de texto

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,6 +1,6 @@
 import { Input, InputProps } from "@chakra-ui/react";
 // O componente CustomLabel é uma função que recebe props do tipo InputProps, isso significa que o CustomLabel recebe apenas parametros que um input geralmente receberia.
-// O ...props permite que outras propriedades sejam passadas para o componente Text caso seja necessário. Teste no LoginPage colocar parametros como _hove={{bg: "#fff"}} na tag CustomInput e você verá que o input mudará de background ao passar o mouse por cima mesmo sem ter sido declarado aqui.
+// O ...props permite que outras propriedades sejam passadas para o componente CustomInput caso seja necessário. Teste no LoginPage colocar parametros como _hove={{bg: "#fff"}} na tag CustomInput e você verá que o input mudará de background ao passar o mouse por cima mesmo sem ter sido declarado aqui.
 export function CustomInput(props: InputProps) {
   return (
     <Input
